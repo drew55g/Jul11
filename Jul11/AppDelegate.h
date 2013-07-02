cd194277800df75d49088bb0e59c9d46c7656a3c
Andrew Rodney <drew_55g@yahoo.com>
2013-07-02 14:30:05 -0400

    Initial Commit

---
+++ Jul11/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Jul11
+//
+//  Created by andrew rodney on 7/2/13.
+//  Copyright (c) 2013 andrew rodney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Jul11/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Jul11
+//
+//  Created by andrew rodney on 7/2/13.
+//  Copyright (c) 2013 andrew rodney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
